5f93d1b043e253dbd64581d1bfa3d48e51fc7353
Shasank Periwal <shasank.periwal@lumiq.ai>
2025-05-17 19:20:40 +0530

    base commit

--- .aws-sam/build/Lambda2Function/app.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def handler(event, context):
-    for record in event['Records']:
-        body = record['body']
-        print("Received message:", body)
-    return {"status": "processed"}
